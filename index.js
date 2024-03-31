974f703d51c20b6c09dec025e110cba714f7a87a
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2024-03-31 13:56:29 -0300

    Init project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app; // Exporting app for testing purposes
